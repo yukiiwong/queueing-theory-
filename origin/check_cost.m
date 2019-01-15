d325f6341a37b3e61da44567d2962c3babe3fd24
a_kkk_service <38659683+yukiiwong@users.noreply.github.com>
2019-01-15 19:58:43 +0800

    Add files via upload

---
+++ origin/check_cost.m
@@ -0,0 +1,17 @@
+function [passenger, finish_time, wait_line] = check_cost(finish_time, finish_people, ser_time, passenger, i, wait_line)
+
+a = finish_time(1, 1);
+b = 0;
+for j = 1:3
+    if finish_time(1, j) <= a
+        a = finish_time(1, j);
+        b = j;
+    end
+end
+finish_time(1, b) = finish_time(1, b) + ser_time(1, finish_people);
+if finish_time >= i + 1
+    wait_line(i + 1, 1) = wait_line(i + 1, 1) + 1;
+end
+passenger(finish_people, 2) = finish_time(1, b);
+end
+    
